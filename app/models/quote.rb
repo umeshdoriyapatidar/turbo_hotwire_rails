2c3e79d6faace0ee0350db118fca51a2a8bec4e6
umeshdoriya <umeshdoriyapatidar@gmail.com>
2024-11-02 17:12:40 +0530

    add turbo stream for realtime upgrade with turbo stream

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,3 +1,11 @@
 class Quote < ApplicationRecord
   validates :name, presence: true
+  scope :ordered, -> { order(id: :desc) }
+  # after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
+  # after_update_commit -> { broadcast_replace_to "quotes" }
+  # after_create_commit -> { broadcast_prepend_later_to "quotes" }
+  # after_update_commit -> { broadcast_replace_later_to "quotes" }
+  # after_destroy_commit -> { broadcast_remove_to "quotes" }
+  # Above three call back are equivalent to the following sinle line of lambda
+  broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
 end
